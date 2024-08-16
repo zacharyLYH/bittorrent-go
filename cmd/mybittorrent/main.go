80ef6a94bca212c27221878d14cc4036baee6fd0
zac <lee.hong@accenture.com>
2024-08-16 11:07:33 +0800

    change implementation to use bencode library instead

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,78 +1,33 @@
 package main
 
 import (
-	// Uncomment this line to pass the first stage
-
+	"bytes"
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
-	"unicode"
 
 	bencode "github.com/jackpal/bencode-go"
 )
 
 // TorrentInfo represents the "info" field in the Bencoded file
 type TorrentInfo struct {
-	Length      int    `json:"length"`
-	Name        string `json:"name"`
-	PieceLength int    `json:"piece length"`
-	Pieces      string `json:"pieces"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
+	PieceLength int    `bencode:"piece length"`
+	Pieces      string `bencode:"pieces"`
 }
 
 // TorrentFile represents the entire Bencoded file
 type TorrentFile struct {
-	Announce  string      `json:"announce"`
-	CreatedBy string      `json:"created by"`
-	Info      TorrentInfo `json:"info"`
+	Announce string      `bencode:"announce"`
+	Info     TorrentInfo `bencode:"info"`
 }
 
-func decodeList(bencodedString string) ([]any, error) {
-	list, err := bencode.Decode(strings.NewReader(bencodedString))
-	return list.([]any), err
-}
-
-func decodeDict(bencodedString string) (map[string]interface{}, error) {
-	list, err := bencode.Decode(strings.NewReader(bencodedString))
-	return list.(map[string]interface{}), err
-}
-
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if bencodedString[0] == 'l' {
-		return decodeList(bencodedString)
-	} else if bencodedString[0] == 'd' {
-		return decodeDict(bencodedString)
-	} else if bencodedString[0] == 'i' && bencodedString[len(bencodedString)-1] == 'e' {
-		numberStr := bencodedString[1 : len(bencodedString)-1]
-		number, err := strconv.Atoi(numberStr)
-		if err != nil {
-			return nil, err
-		}
-		return number, nil
-	} else if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
-
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else {
-		return "", fmt.Errorf("only strings are supported at the moment")
+func handleErrorGeneric(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
 
@@ -86,7 +41,7 @@ func main() {
 
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, err := bencode.Decode(bytes.NewReader([]byte(bencodedValue)))
 
 		handleErrorGeneric(err)
 
@@ -94,19 +49,22 @@ func main() {
 		fmt.Println(string(jsonOutput))
 
 	} else if command == "info" {
-
-		torrentFile := os.Args[2]
-
-		fileData, err := readFileReturnBytes(torrentFile)
-
-		handleErrorGeneric(err)
-
-		decoded, err := decodeBencodeToTorrentFile(string(fileData))
-
-		handleErrorGeneric(err)
-
-		fmt.Printf("Tracker URL: %s\n", decoded.Announce)
-		fmt.Printf("Length: %d", decoded.Info.Length)
+		fileName := os.Args[2]
+		f, err := os.Open(fileName)
+		if err != nil {
+			panic(err)
+		}
+		var meta TorrentFile
+		if err := bencode.Unmarshal(f, &meta); err != nil {
+			panic(err)
+		}
+		fmt.Println("Tracker URL:", meta.Announce)
+		fmt.Println("Length:", meta.Info.Length)
+		h := sha1.New()
+		if err := bencode.Marshal(h, meta.Info); err != nil {
+			panic(err)
+		}
+		fmt.Printf("Info Hash: %x\n", h.Sum(nil))
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
